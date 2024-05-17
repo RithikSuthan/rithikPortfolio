9fe51a850fb2cb147935102bb40ca867b622fb95
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-05-17 13:12:03 +0530

    added projects and updated skills

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -42,9 +42,49 @@ export class ProjectsComponent implements OnInit {
       const url = 'https://github.com/RithikSuthan/Make-My-Journey';
       window.open(url, '_blank');
   }
+  openStaffSyncGithub()
+  {
+      const url = 'https://github.com/RithikSuthan/employeemanagementUI';
+      window.open(url, '_blank');
+  }
+  openStaffSync()
+  {
+      const url = 'https://employeeimage-latest.onrender.com';
+      window.open(url, '_blank');
+  }
   openCarPrice()
   {
-    const url = 'https://github.com/RithikSuthan/Car-Price_Prediction';
+    const url = 'https://github.com/RithikSuthan/Car-Price-Prediction';
+    window.open(url, '_blank');
+  }
+  openMelinia()
+  {
+    const url = 'https://melinia24.tech';
+    window.open(url, '_blank');
+  }
+  openMeliniaGithub()
+  {
+    const url = 'https://github.com/RithikSuthan/Melinia2K24';
+    window.open(url, '_blank');
+  }
+  openFire()
+  {
+    const url = 'https://drive.google.com/file/d/1WjfXlG6cLh9b0m37j0sO0oxnLcZ6L1Nw/view?usp=sharing';
+    window.open(url, '_blank');
+  }
+  openFireGithub()
+  {
+    const url = 'https://github.com/RithikSuthan/Fire-Fighting-Robot';
+    window.open(url, '_blank');
+  }
+  openCrimeGithub()
+  {
+    const url = 'https://github.com/RithikSuthan/Analysis-of-Crime-Against-Women-in-India';
+    window.open(url, '_blank');
+  }
+  github()
+  {
+    const url = 'https://github.com/RithikSuthan';
     window.open(url, '_blank');
   }
 }
