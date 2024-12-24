4069cd1cd61ca368f99161f012328d007ff6956e
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-12-24 21:48:49 +0530

    updated from gcloud to render email id

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -11,7 +11,8 @@ export class MailService {
   email_service_url:any;
 
   constructor(private http:HttpClient) { 
-    this.email_service_url="https://mailboxexpress.el.r.appspot.com";
+    // this.email_service_url="https://mailboxexpress.el.r.appspot.com";
+    this.email_service_url="https://mailbox-express-k9hn.onrender.com";
     // this.email_service_url="http://127.0.0.1:5000";
     
   }
